16ab5b40444ee53486b9852f0edb34e522aa6f97
Omnia Salama <omniamsalama25@gmail.com>
2025-09-24 17:22:39 +0300

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,5 +4,5 @@ export const serverRoutes: ServerRoute[] = [
   { path: '**', renderMode: RenderMode.Server },
   { path: 'address/:cartId', renderMode: RenderMode.Server }, // keep server render
   { path: 'details/:id', renderMode: RenderMode.Server }, // keep server render
-  { path: 'allorders', renderMode: RenderMode.Client },
+  { path: 'allorders', renderMode: RenderMode.Server },
 ];
